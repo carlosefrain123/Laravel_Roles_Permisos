bb916a22f68ea8c735598771827be21cb440edb2
Carlos Efrain Chafloque LLontop <122811704+carlosefrain123@users.noreply.github.com>
2024-03-09 01:30:23 -0500

    09/03/2024 - 01:29 - Se agrega el proyecto en el controlador y a la vista show el post que al dar click, se mnada a su mismo post

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -7,11 +7,19 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    public function index(){
-        $posts=Post::where('status',2)->latest('id')->paginate(8);
-        return view('posts.index',compact('posts'));
+    public function index()
+    {
+        $posts = Post::where('status', 2)->latest('id')->paginate(8);
+        return view('posts.index', compact('posts'));
     }
-    public function show(Post $post){
-        return view('posts.show',compact('post'));
+    public function show(Post $post)
+    {
+        $similares = Post::where('categoria_id', $post->categoria_id)
+            ->where('status', 2)
+            ->where('id','!=',$post->id)
+            ->latest('id')
+            ->take(4)
+            ->get();
+        return view('posts.show', compact('post', 'similares'));
     }
 }
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,24 +1,36 @@
 <x-app-layout>
     <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8">
         <h1 class="text-4xl font-bold text-gray-600">
-            {{$post->name}}
+            {{ $post->name }}
         </h1>
         <div class="text-lg text-gray-500 mb-2">
-            {{$post->extract}}
+            {{ $post->extract }}
         </div>
-        <div class="grid grid-cols-3">
+        <div class="grid grid-cols-1 lg:grid-cols-3 gap-6">
             {{-- Contenido Principal --}}
-            <div class="col-span-2">
+            <div class="lg:col-span-2">
                 <figure>
-                    <img class="w-full h-80 object-cover object-center" src="{{Storage::url($post->image->url)}}" alt="">
+                    <img class="w-full h-80 object-cover object-center" src="{{ Storage::url($post->image->url) }}"
+                        alt="">
                 </figure>
                 <div class="text-base text-gray-500 mt-4">
-                    {{$post->body}}
+                    {{ $post->body }}
                 </div>
             </div>
             {{-- Contenido relacionado --}}
             <aside>
-
+                <h1 class="text-2xl font-bold text-gray-600  mb-4">Más en {{ $post->categoria->name }}</h1>
+                <ul>
+                    @foreach ($similares as $similar)
+                        <li class="mb-4">
+                            <a class="flex" href="{{ route('posts.show', $similar) }}">
+                                <img class="w-36 h-20 object-cover object-center"
+                                    src="{{ Storage::url($similar->image->url) }}" alt="">
+                                <span class="ml-2 text-gray-600">{{ $similar->name }}</span>
+                            </a>
+                        </li>
+                    @endforeach
+                </ul>
             </aside>
         </div>
     </div>
