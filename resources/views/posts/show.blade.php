df12c9cc01139ed8adcc05dec632ee0950e4e773
Carlos Efrain Chafloque LLontop <122811704+carlosefrain123@users.noreply.github.com>
2024-03-09 00:42:08 -0500

    09/03/2024 - 00:41 - Se agrega la interfaz de show, ya que en el ello viene hacer el id correspondiente del index principal

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,3 +1,25 @@
 <x-app-layout>
-    
+    <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 py-8">
+        <h1 class="text-4xl font-bold text-gray-600">
+            {{$post->name}}
+        </h1>
+        <div class="text-lg text-gray-500 mb-2">
+            {{$post->extract}}
+        </div>
+        <div class="grid grid-cols-3">
+            {{-- Contenido Principal --}}
+            <div class="col-span-2">
+                <figure>
+                    <img class="w-full h-80 object-cover object-center" src="{{Storage::url($post->image->url)}}" alt="">
+                </figure>
+                <div class="text-base text-gray-500 mt-4">
+                    {{$post->body}}
+                </div>
+            </div>
+            {{-- Contenido relacionado --}}
+            <aside>
+
+            </aside>
+        </div>
+    </div>
 </x-app-layout>
