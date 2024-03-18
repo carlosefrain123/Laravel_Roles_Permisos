c2919004c2fe5130327715c89ace729087e640dd
Carlos Efrain Chafloque LLontop <122811704+carlosefrain123@users.noreply.github.com>
2024-03-18 09:43:21 -0500

    18/03/2024 - 09:42 - Se modifica la interfaz create donde se visualiza el input y label

--- resources/views/admin/categorias/create.blade.php
+++ resources/views/admin/categorias/create.blade.php
@@ -9,24 +9,16 @@
 @section('content')
     <div class="card">
         <div class="card-body">
-            <form action="{{-- {{route('admin.categorias.store')}} --}}" method="post">
-                @csrf
-                <div class="form-group row">
-                    <label for="name" class="col-sm-2 col-form-label">Nombre de la categoría</label>
-                    <div class="col-sm-10">
-                        <input type="text" class="form-control" id="name" name="name" value="{{old('name')}}">
-                        @error('name')
-                            <span class="text-danger">
-                                <span>*{{$message}}</span>
-                            </span>
-                        @enderror
-                    </div>
+            {!! Form::open(['route' => 'admin.categorias.store']) !!}
+                <div class="form-group">
+                    {!! Form::label('name', 'Nombre') !!}
+                    {!! Form::text('name', null, ['class'=>'form-control','placeholder'=>"Ingrese el nombre"]) !!}
                 </div>
-                <div class="text-center">
-                    <input type="submit" value="crear especialidad" class="btn btn-primary">
-                    <a href="{{route('admin.categorias.index')}}" class="btn btn-danger">Cancelar</a>
+                <div class="form-group">
+                    {!! Form::label('slug', 'Slug') !!}
+                    {!! Form::text('name', null, ['class'=>'form-control','placeholder'=>"Ingrese el slug"]) !!}
                 </div>
-            </form>
+            {!! Form::close() !!}
         </div>
     </div>
 @stop
